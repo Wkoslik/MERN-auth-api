f5d5f778925400635fdf12c14dbedee87bd6b8c7
wkoslik <wkoslik@gmail.com>
2021-02-17 19:46:56 -0500

    passport jwt ftw

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,33 @@
+const bcrypt = require('bcrypt')
 const express = require('express')
 const db = require('../models')
 const router = express.Router()
 
 //URL prefix - '/api'
 
+
 //signup - POST /api/signup
 router.post('/signup', (req,res) =>{
-    // res.json({ message: 'SIGNUP POST'})
-    db.User.create(req.body)
-    .then(user =>{
-        res.json(user)
-    })
+    bcrypt.hash(req.body.password, 10) //do 10, 11, or 12
+    .then(hash => db.User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+    }))
+    .then(createdUser => res.json(createdUser))
     .catch(err =>{
-        console.log('🔥 Error in the Post Signup', err)
-        res.json({ 'error': err })
+        console.log(`🔥 error in the post signup`, err)
+        res.json({ error: err})
     })
+    // // res.json({ message: 'SIGNUP POST'})
+    // db.User.create(req.body)
+    // .then(user =>{
+    //     res.json(user)
+    // })
+    // .catch(err =>{
+    //     console.log('🔥 Error in the Post Signup', err)
+    //     res.json({ 'error': err })
+    // })
 })
 
 //login POST /api/login
